40b7636ab5e3b85690c24cd0b19213e4f1f66e88
wzh-company <zihengwang926@gmail>
2025-12-31 15:56:51 +0800

    变量读取mysql、redis配置

--- database/db.go
+++ database/db.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/canary-debug/kube-vue-admin/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,7 +12,23 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "root:1qazWSX!@tcp(43.138.164.205:30001)/kube?charset=utf8mb4&parseTime=True&loc=Local"
+	// 从环境变量读取 MySQL user password
+	MySQL_User := os.Getenv("MYSQL_USER")
+	MySQL_Password := os.Getenv("MYSQL_PASSWORD")
+	MySQL_Host := os.Getenv("MYSQL_HOST")
+	MySQL_Port := os.Getenv("MYSQL_PORT")
+	MySQL_DBName := os.Getenv("MYSQL_DBNAME")
+
+	// 动态拼接 DSN
+	// 格式: 用户名:密码@tcp(地址)/数据库名?参数
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		MySQL_User,
+		MySQL_Password,
+		MySQL_Host,
+		MySQL_Port,
+		MySQL_DBName,
+	)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
--- database/redisdb.go
+++ database/redisdb.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -10,10 +12,16 @@ import (
 var RC *redis.Client
 
 func ConnectRedis() {
+	// 从环境变量读取 Redis 配置
+	Redis_Host := os.Getenv("REDIS_HOST")
+	Redis_Password := os.Getenv("REDIS_PASSWORD")
+	Redis_DB := os.Getenv("REDIS_DB")
+	// 把 Redis_DB 转化为 int 类型
+	Redis_DBInt, _ := strconv.Atoi(Redis_DB)
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:         "43.138.164.205:30007",
-		Password:     "1qazWSX!",
-		DB:           9,
+		Addr:         Redis_Host,
+		Password:     Redis_Password,
+		DB:           Redis_DBInt,
 		WriteTimeout: 1 * time.Second, // 写入超时设置为1秒
 		ReadTimeout:  1 * time.Second, // 读取超时设置为1秒
 		DialTimeout:  3 * time.Second, // 建立连接的超时时间也可以适当调整
